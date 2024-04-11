43d91fb5203087a99de340b6d38548e5d8f6a2f5
oliviarose728 <oliviarose728@gmail.com>
2024-04-10 20:59:15 -0400

    added mobile touch functionality

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ const slots = document.querySelectorAll(".slot");
 const letters = document.querySelectorAll(".letter");
 const resetButton = document.getElementById("resetButton");
 
+let touchedElementId;
+
 letters.forEach(element =>{
     element.addEventListener("dragstart", dragStart);
+    element.addEventListener("touchstart", letterTouchStart);
 })
 
 slots.forEach(element =>{
     element.addEventListener("dragover", dragOver);
     element.addEventListener("drop", drop);
+    element.addEventListener("touchstart", slotTouchStart);
 })
 
 resetButton.addEventListener("click", reset);
 
+function letterTouchStart(event){
+
+    console.log("Letter touched:", event.currentTarget.id);
+    touchedElementId = event.currentTarget.id;
+}
+
+function slotTouchStart(event){
+
+    event.preventDefault();
+    const sourceID = touchedElementId;
+    const sourceImg = document.getElementById(sourceID).querySelector('img');
+    const src = sourceImg.src;
+    event.target.insertAdjacentHTML("afterbegin", `<div class="letter dragged" id="${sourceID}"><img src="${src}" class="gridletterimg"></div>`);
+}
 
 function dragStart(event){
     event.dataTransfer.setData("text", event.target.id);
